6f1cf3f920ebf06b262e5e0b2404f494dd20aac2
jegasape <jeffrey@crecerlab.com>
2024-08-21 11:22:29 -0400

    feat(exchange): add-currency-exchange

---
+++ tasa.sh
@@ -0,0 +1,9 @@
+curl 'https://www.banreservas.com/_layouts/15/SharePointAPI/ObtenerTasas.ashx' \
+  -H 'sec-ch-ua: "Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36' \
+  -H 'x-dtpc: 29$452749211_265h12vFEMRMHKVGVJCOUVOJFPTQHUFHQRJGARO-0e0' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Referer: https://www.banreservas.com/' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'sec-ch-ua-platform: "Windows"'
